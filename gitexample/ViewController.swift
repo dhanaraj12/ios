1b73efcf80a5c3129958ddfca513d5015da0e2f7
dhanaraj <dhanaraj.ashokkumar@gmail.com>
2017-02-01 21:53:24 +0530

    Initial Commit

---
+++ gitexample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gitexample
+//
+//  Created by Ava Mac 1 on 2/1/17.
+//  Copyright © 2017 Ava Mac 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
